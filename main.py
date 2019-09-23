0c29befe54eaf256f5d43fded2c11dbc7dd7b1ac
Juan Pleitez <juanpc13lolol@gmail.com>
2019-09-23 11:13:44 -0600

    Busqueda del dispositivo

--- main.py
+++ main.py
@@ -1,39 +1,36 @@
+import time
+import ubinascii
 import lib.Micropg.micropg as micropg
-from lib.MicroWebSrv.microWebSrv import MicroWebSrv
 
-# ------------------WebSocket------------------------------------
-def _acceptWebSocketCallback(webSocket, httpClient) :
-	print("WS ACCEPT")
-	webSocket.RecvTextCallback   = _recvTextCallback
-	webSocket.RecvBinaryCallback = _recvBinaryCallback
-	webSocket.ClosedCallback 	 = _closedCallback
+mac = ubinascii.hexlify(network.WLAN().config('mac'),':').decode()
+conn = micropg.connect(host='35.235.111.68', user='postgres', password='Cal15!', database='prototipo2', use_ssl=False)
 
-def _recvTextCallback(webSocket, msg) :
-	print("WS RECV TEXT : %s" % msg)
-	webSocket.SendText("Reply for %s" % msg)
-
-def _recvBinaryCallback(webSocket, data) :
-	print("WS RECV DATA : %s" % data)
-
-def _closedCallback(webSocket) :
-	print("WS CLOSED")
-# -----------------------------------------------------------------
+#Check this Device
+id_dispositivo = None
+if conn.is_connect():
+	#Find
+	cur = conn.cursor()
+	cur.execute("SELECT d.id_dispositivo FROM dispositivo as d WHERE d.mac='"+mac+"'")
+	id_dispositivo = cur.fetchone()
+	if id_dispositivo is not None:
+		id_dispositivo = id_dispositivo[0]
+		print(id_dispositivo)
+else:
+	print("No Conected")
 
 
+while conn.is_connect() and id_dispositivo is not None:
 
-mws = MicroWebSrv(webPath="/webapp")                    # TCP port 80 and files in /flash/www
-mws.MaxWebSocketRecvLen     = 256                       # Default is set to 1024
-mws.WebSocketThreaded       = False                     # WebSockets without new threads
-mws.AcceptWebSocketCallback = _acceptWebSocketCallback  # Function to receive WebSockets
-mws.Start(threaded=True)                                # Starts server in a new thread
+	if conn.is_connect():
+		cur = conn.cursor()
+		cur.execute('select * from acelerometro')
+		for r in cur.fetchall():
+   			print("Data is : ",r[0], r[1], r[2], r[3], r[4])
+	else:
+		print("No Conected")
+		
+	#Delay de 1 segundo
+	time.sleep(1)
 
-'''conn = micropg.connect(host='35.235.111.68', user='postgres', password='Cal15!', database='prototipo2', use_ssl=False)
-
-cur = conn.cursor()
-cur.execute('select * from acelerometro')
-for r in cur.fetchall():
-   print(r[0], r[1], r[2], r[3])
-
-
+print("Cerrando Conexion")
 conn.close()
-'''
