8b5a18fc292a7a2b67a4573a827794ce7cb9f403
Juan Pleitez <juanpc13lolol@gmail.com>
2019-09-27 02:46:14 -0600

    Implementando co2 y humedad

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ conn = micropg.connect(host='35.235.111.68', port=1000,user='postgres', password
 
 #Licor Variable
 licorCounter = 0 # Contador de la licor en segundos
-licorStart = 60*8 # Encender en el minuto 8 y expresarlo en segundos
-licorCollectData = 60*9 # Recolectar datos desde el minuto 9 y expresarlo en segundos
+licorStart = 60*7 # Encender en el minuto 7 y expresarlo en segundos
+licorCollectData = 60*8 # Recolectar datos desde el minuto 8 y expresarlo en segundos
 licorDelay = 60*10 # 10 Minutos totales para cada dato y expresarlo en segundos
-licorPower = Pin(2, Pin.OUT)
+licorPower = Pin(13, Pin.OUT)
 licorPins = [ADC(Pin(35)), ADC(Pin(32))]
 #Acelerometro Variable
 acelerometroPins = [ADC(Pin(36)), ADC(Pin(39)), ADC(Pin(34))]
@@ -27,13 +27,31 @@ for p in acelerometroPins:
 
 #Ecuacion para Acelerometro
 def acelerometroConversion(x):
-  x1 = 1400.0
-  y1 = -9.8
-  x2 = 2175.0
-  y2 = 9.8
-  y = 0.0
-  y = ((y2-y1)/(x2-x1))*(x-x1)+y1
-  return y
+	x1 = 1400.0
+	y1 = -9.8
+	x2 = 2175.0
+	y2 = 9.8
+	y = 0.0
+	y = ((y2-y1)/(x2-x1))*(x-x1)+y1
+	return y
+
+def map(value, leftMin, leftMax, rightMin, rightMax):
+	# Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return rightMin + (valueScaled * rightSpan)
+
+def constrain(value, min, max):
+	if value <= min:
+		value = min
+	elif value >= max:
+		value = max
+	return value
 
 def sendQuery(conn, query):
 	cur = conn.cursor()		
@@ -93,15 +111,19 @@ while id_dispositivo is not None:
 			query += "INSERT INTO co2(id_dispositivo, ppm) VALUES(?,?);"
 			query = query.replace('?',str(id_dispositivo), 1)
 			value = licorPins[0].read()
+			value = map(value, 745, 3722, 0, 20000)
+			value = constrain(value, 0, 20000)
 			query = query.replace('?',str(value), 1)
 
 			#Datos rH
 			query += "INSERT INTO h2o(id_dispositivo, rh) VALUES(?,?);"
 			query = query.replace('?',str(id_dispositivo), 1)
 			value = licorPins[1].read()
-			query = query.replace('?',str(value), 1)			
+			value = map(value, 745, 3722, 0, 60)
+			value = constrain(value, 0, 60)
+			query = query.replace('?',str(value), 1)
 	else:
-		licorPower.off()	
+		licorPower.off()
 
 	#Envio de los datos
 	sendQuery(conn, query)
